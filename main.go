a9c2068c90a92f9263ac24418b9e58edd351ee2e
robodo <richieobodo1738@gmail.com>
2026-07-31 11:23:32 +0100

    Add student/teacher profile page with GPA, department, and class info

--- main.go
+++ main.go
@@ -20,6 +20,18 @@ func main() {
 
 	db.Connect()
 
+	schemaSQL, err := os.ReadFile("schema.sql")
+	if err != nil {
+    	log.Println("Could not read schema.sql:", err)
+	} else {
+    	_, err = db.DB.Exec(string(schemaSQL))
+    	if err != nil {
+        	log.Println("Schema execution error:", err)
+    	} else {
+        	log.Println("Schema applied successfully")
+    	}
+	}
+
 	mux := routes.RegisterRoutes()
 
 	fs := http.FileServer(http.Dir("./static"))
